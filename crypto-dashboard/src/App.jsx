307f2bb2bfbf2dba5e618d486687747f6b51920a
Ritik Satokar <ritiksatokar2000@gmail.com>
2026-03-06 14:37:58 +0530

    Add styling and Router

--- crypto-dashboard/src/App.jsx
+++ crypto-dashboard/src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   
   return (
     <>
-     
+     <Navbar/>
+     <div className="container">
+      <Dashboard/>
+     </div>
     </>
   )
 }
--- crypto-dashboard/src/main.jsx
+++ crypto-dashboard/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Dashboard from "./Components/Dashboard.jsx";
+import Wishlist from "./Components/Wishlist.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/wishlist", element: <Wishlist /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+  
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
-)
+);
