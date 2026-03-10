99ba5cc9a2c98dd881f9d550806fbe0296c4fa41
Ritik Satokar <ritiksatokar2000@gmail.com>
2026-03-10 10:51:51 +0530

    File are Change

--- crypto-dashboard/src/Components/CrytoChart.jsx
+++ crypto-dashboard/src/Components/CrytoChart.jsx
@@ -1,26 +1,55 @@
 import { DataGrid } from "@mui/x-data-grid";
 
-const CrytoChart = () => {
+const CrytoChart = ({ coin, setSelectedCoin }) => {
+  const rows = coin.map((c, index) => ({
+    id: c.id,
+    srno: index + 1,
+    img: c.image,
+    coin: c.name,
+    price: `$${c.current_price}`,
+    change: `${(c.price_change_percentage_24h ?? 0).toFixed(2)}%`,
+    marketCap: `$${c.market_cap}`,
+  }));
+
+  const columns = [
+    { field: "srno", headerName: "SrNo.", width: 50 },
+    {
+      field: "img",
+      headerName: "",
+      width: 60,
+      renderCell: (params) => <img src={params.value} width={25} />,
+    },
+    { field: "coin", headerName: "Coin", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+    { field: "change", headerName: "24h Change", width: 150 ,
+      renderCell:(params) => {
+        const value = parseFloat(params.value);
+        return(
+          <span style={{color: value > 0 ? "green" : "red"}}>
+{params.value}
+          </span>
+        )
+      }
+    },
+    { field: "marketCap", headerName: "Market Cap", width: 180 },
+  ];
+
   return (
     <div className="crytoChart ">
       <DataGrid
-        columns={[
-          { field: "coin", headerName: "Coin" },
-          { field: "price", headerName: "Price" },
-          { field: "change", headerName: "24h Change", width: 120 },
-          { field: "marketCap", headerName: "2Market Cap", width: 150 },
-        ]}
-        rows={[
-          {
-            id: 1,
-            coin: "BitCoin",
-            price: "$42,580.15",
-            change: "+3.25%",
-            marketCap: "$803.58",
+        columns={columns}
+        rows={rows}
+        onRowClick={(params) => {
+          setSelectedCoin(params.row);
+        }}
+        pageSizeOptions={[10]}
+        initialState={{
+          pagination: {
+            paginationModel: { pageSize: 10, page: 0 },
           },
-        ]}
+        }}
         // slots={{ noRowsOverlay: CustomNoRowsOverlay }}
-        sx={{ "--DataGrid-overlayHeight": "500px" }}
+        sx={{ "--DataGrid-overlayHeight": "100px" }}
       />
     </div>
   );
--- crypto-dashboard/src/Components/CrytoHeader.jsx
+++ crypto-dashboard/src/Components/CrytoHeader.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
+
 const CrytoHeader = () => {
+  //data.data.market_cap_percentage.btc)
+  const [globalData, setGlobalData] = useState(null);
+
+  function formatNumber(num) {
+  if (num >= 1e12) return (num / 1e12).toFixed(2) + "T";
+  if (num >= 1e9) return (num / 1e9).toFixed(2) + "B";
+  if (num >= 1e6) return (num / 1e6).toFixed(2) + "M";
+  return num;
+}
+
+
+  useEffect(() => {
+    fetch("https://api.coingecko.com/api/v3/global")
+      .then((res) => res.json())
+      .then((data) => {
+        setGlobalData(data.data);
+      });
+  }, []);
+
+  if(!globalData){
+    return <p>Loading...</p>
+  }
+
+
   return (
-    <div className="crytoHeader ">
+    <div className="crytoHeader">
       <div class="row ">
         <div class="col">
           <div class="card" style={{ width: "18rem" }}>
             <div class="card-body">
               <h5 class="card-title">Total Market Cap</h5>
-              <h2 class="card-text">$1.75 Trillion</h2>
+              <h2 class="card-text"> ${formatNumber(globalData.total_market_cap.usd)}</h2>
             </div>
           </div>
         </div>
@@ -14,7 +40,7 @@ const CrytoHeader = () => {
           <div class="card" style={{ width: "18rem" }}>
             <div class="card-body">
               <h5 class="card-title">24h Volume</h5>
-              <h2 class="card-text">$98.4 Billion</h2>
+              <h2 class="card-text">${formatNumber(globalData.total_volume.usd)}</h2>
             </div>
           </div>
         </div>
@@ -22,7 +48,7 @@ const CrytoHeader = () => {
           <div class="card" style={{ width: "18rem" }}>
             <div class="card-body">
               <h5 class="card-title">BTC Dominance</h5>
-              <h2 class="card-text">42.5%</h2>
+              <h2 class="card-text"> {globalData.market_cap_percentage.btc.toFixed(2)}%</h2>
             </div>
           </div>
         </div>
